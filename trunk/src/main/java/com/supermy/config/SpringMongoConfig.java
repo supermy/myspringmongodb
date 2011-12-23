cd5954d466aaaeee81e97d7bbf216728e6d68c0e
springclick@gmail.com <springclick@gmail.com@9380c161-f7de-78aa-f678-0a84a6d65ec5>
2011-12-23 16:54:28 +0000

    mongo

---
+++ trunk/src/main/java/com/supermy/config/SpringMongoConfig.java
@@ -0,0 +1,28 @@
+package com.supermy.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.document.mongodb.MongoTemplate;
+import org.springframework.data.document.mongodb.config.AbstractMongoConfiguration;
+import com.mongodb.Mongo;
+
+/**
+ * Spring MongoDB configuration file
+ * 
+ */
+@Configuration
+public class SpringMongoConfig extends AbstractMongoConfiguration {
+
+	@Override
+	public @Bean Mongo mongo() throws Exception {
+
+		return new Mongo("localhost");
+	}
+
+	@Override
+	public @Bean MongoTemplate mongoTemplate() throws Exception {
+
+		return new MongoTemplate(mongo(),"yourdb","yourCollection");
+	}
+		
+}
